b8dedbac0d9b6ca6a6c613abd2ef688c920e874d
Soledad <48894797+soledadAmore@users.noreply.github.com>
2020-04-12 15:59:23 -0300

    Separación de clases
    Cada clase en un proyecto distinto

---
+++ Validar contrasenias/src/ArchivoContraseniasInvalidas.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class ArchivoContraseniasInvalidas {
+	public static boolean encontrar(String contraseniaInvalida) {
+        File archivo = null;
+        FileReader lectorCaracter = null; // lee caracter a caracter
+        BufferedReader lectorPalabra = null; // lee una palabra al detectar un \n
+
+          try {                                       
+             archivo = new File("contrasenias10000.txt");  
+             lectorCaracter = new FileReader (archivo);           
+             lectorPalabra = new BufferedReader(lectorCaracter);            
+             String linea;
+             while((linea=lectorPalabra.readLine())!=null)// mientras no lleguemos al final del archivo
+                if(linea.contentEquals(contraseniaInvalida))
+                {
+                    return linea.contentEquals(contraseniaInvalida);
+                }
+
+          }
+          catch(Exception e){
+             e.printStackTrace();
+          }finally{
+             // cierra el archivo
+             try{
+                if( lectorCaracter !=  null ){
+                   lectorCaracter.close();
+                }
+             }catch (Exception e2){ 
+                e2.printStackTrace();
+             }
+          }
+        return false;
+       }
+}

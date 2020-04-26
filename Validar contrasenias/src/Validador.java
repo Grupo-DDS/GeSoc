291517274abf4a7f88199d9729062f1fa3d160ee
MarcosRivitti <marcosrivitti77@gmail.com>
2020-04-26 18:26:11 -0300

    Delete Validador.java

--- Validar contrasenias/src/Validador.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class Validador{
-	public static void main(String[] args){
-		String contrasenia = JOptionPane.showInputDialog("Ingrese su contraseña");
-		ReguladorDeContrasenias.regular(contrasenia);	 
-	}	
-}

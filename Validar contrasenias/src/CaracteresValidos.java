2105a9186b014456b03449cb46a15a312c061c89
juan061099 <49323627+juan061099@users.noreply.github.com>
2020-04-27 16:43:07 -0300

    Agrego la carpeta src

---
+++ Validar contrasenias/src/CaracteresValidos.java
@@ -0,0 +1,14 @@
+//package com.validarContraseñas;
+
+public class CaracteresValidos{
+	static String listaCararcteresValidos = " !\",#$%&()*+-,-./0123456789:;<=>ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`@abcdefghijklmnopqrstuvwxyz{}";
+	static public boolean validar(String contrasenia){
+		int i=0;
+		for(i=0;i<contrasenia.length();i++){
+			if(!listaCararcteresValidos.contains(Character.toString(contrasenia.charAt(i)))) {
+				return false;
+			}	
+		}
+		return true;
+	}
+}
---
+++ Validar contrasenias/src/Validador.java
@@ -0,0 +1,24 @@
+//package com.validarContraseñas;
+import javax.swing.JOptionPane;
+
+public class Validador{
+	static String mensaje= "Contraseña invalida\n";
+	
+	public static void main(String[] args){
+		String contrasenia = JOptionPane.showInputDialog("Ingrese su contraseña");
+		
+		if(!LongitudValida.validar(contrasenia))
+			mensaje += "Su contraseña es demasiado corta\n";
+		if(!CaracteresValidos.validar(contrasenia))
+			mensaje += "Utilice caracteres validos\n";
+		if(ArchivoContraseniasInvalidas.validar(contrasenia))
+			mensaje += "Su contraseña es demasiado frecuente\n ";
+		if(CaracteresRepetidos.validar(contrasenia))
+			mensaje += "Su contraseña tiene muchos caracteres repetidos\n ";
+		if(CaracteresConsecutivos.validar(contrasenia))
+			mensaje += "Su contraseña tiene muchos caracteres consecutivos\n";
+		if(LongitudValida.validar(contrasenia) && CaracteresValidos.validar(contrasenia)  && !ArchivoContraseniasInvalidas.validar(contrasenia) && !CaracteresRepetidos.validar(contrasenia))
+			mensaje = "Contraseña Valida";
+		JOptionPane.showMessageDialog(null,mensaje);
+	}
+}

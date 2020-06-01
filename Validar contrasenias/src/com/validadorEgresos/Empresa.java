bc17dac880a6928dfaf19e776606194552731480
Santiago <Santiago@DESKTOP-GTJKD68>
2020-06-01 16:04:05 -0300

    A testear

--- Validar contrasenias/src/com/validadorEgresos/Empresa.java
+++ Validar contrasenias/src/com/validadorEgresos/Empresa.java
@@ -4,4 +4,24 @@ public class Empresa extends TipoEntidad{
 	int personal;
 	int vtasAnuales;
 	String actividad;
+	
+	public int getPersonal() {
+		return personal;
+	}
+	public void setPersonal(int personal) {
+		this.personal = personal;
+	}
+	public int getVtasAnuales() {
+		return vtasAnuales;
+	}
+	public void setVtasAnuales(int vtasAnuales) {
+		this.vtasAnuales = vtasAnuales;
+	}
+	public String getActividad() {
+		return actividad;
+	}
+	public void setActividad(String actividad) {
+		this.actividad = actividad;
+	}
+	
 }

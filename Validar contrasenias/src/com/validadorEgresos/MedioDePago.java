2380879f6fe800b7fabd9cc5be033e215dd01323
Santiago <SantiagoAspres>
2020-06-02 02:53:38 -0300

    Agregado de clase MedioDePago y a finalizar OperacionEgreso

---
+++ Validar contrasenias/src/com/validadorEgresos/MedioDePago.java
@@ -0,0 +1,7 @@
+package com.validadorEgresos;
+
+public class MedioDePago {
+int id;
+String medio;
+String paymentTypeId;
+}
--- Validar contrasenias/src/com/validadorEgresos/OperacionEgreso.java
+++ Validar contrasenias/src/com/validadorEgresos/OperacionEgreso.java
@@ -2,5 +2,53 @@ package com.validadorEgresos;
 
 public class OperacionEgreso {
 	DocumentoComercial comprobante;
+	int fechaOperacion;
+	MedioDePago medioDePago;
+	Organizacion organizacion;
+	Producto producto;
+	Proveedor proveedor;
+	float valorDeEgreso;
 	
+	public DocumentoComercial getComprobante() {
+		return comprobante;
+	}
+	public void setComprobante(DocumentoComercial comprobante) {
+		this.comprobante = comprobante;
+	}
+	public int getFechaOperacion() {
+		return fechaOperacion;
+	}
+	public void setFechaOperacion(int fechaOperacion) {
+		this.fechaOperacion = fechaOperacion;
+	}
+	public MedioDePago getMedioDePago() {
+		return medioDePago;
+	}
+	public void setMedioDePago(MedioDePago medioDePago) {
+		this.medioDePago = medioDePago;
+	}
+	public Organizacion getOrganizacion() {
+		return organizacion;
+	}
+	public void setOrganizacion(Organizacion organizacion) {
+		this.organizacion = organizacion;
+	}
+	public Producto getProducto() {
+		return producto;
+	}
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+	public Proveedor getProveedor() {
+		return proveedor;
+	}
+	public void setProveedor(Proveedor proveedor) {
+		this.proveedor = proveedor;
+	}
+	public float getValorDeEgreso() {
+		return valorDeEgreso;
+	}
+	public void setValorDeEgreso(float valorDeEgreso) {
+		this.valorDeEgreso = valorDeEgreso;
+	}
 }

1ea3190eac17b2cdc23b194b16a3010cd6e342a6
Julián García <49066529+juliangarcia45@users.noreply.github.com>
2020-06-26 18:21:59 -0300

    Add files via upload

---
+++ tp/src/main/java/criteriosCategorias/Categoria.java
@@ -0,0 +1,13 @@
+package criteriosCategorias;
+
+public class Categoria {
+	private CriterioCategorizacion criterio;
+	
+	public CriterioCategorizacion getCriterio() {
+		return criterio;
+	}
+	public void setCriterio(CriterioCategorizacion criterioCategorizacion) {
+		this.criterio = criterioCategorizacion;
+	}
+
+}
---
+++ tp/src/main/java/criteriosCategorias/CriterioCategorizacion.java
@@ -0,0 +1,14 @@
+package criteriosCategorias;
+
+import java.util.ArrayList;
+
+public class CriterioCategorizacion {
+	ArrayList<Categoria> categorias = new ArrayList<Categoria>();
+	public ArrayList<CriterioCategorizacion> criterioPadre = new ArrayList<CriterioCategorizacion>();
+	public ArrayList<CriterioCategorizacion> criterioHijo = new ArrayList<CriterioCategorizacion>();
+	
+	public void agregarCategoria(Categoria categoria) {
+		categorias.add(categoria);
+		categoria.setCriterio(this);
+	}
+}

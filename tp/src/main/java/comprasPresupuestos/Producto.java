b74bf5066ea546668b91fa1c3e0d627ab6757403
Julián García <49066529+juliangarcia45@users.noreply.github.com>
2020-06-26 18:19:06 -0300

    Update Producto.java

--- tp/src/main/java/comprasPresupuestos/Producto.java
+++ tp/src/main/java/comprasPresupuestos/Producto.java
@@ -1,9 +1,14 @@
 package comprasPresupuestos;
 
+import java.util.ArrayList;
+
+import criteriosCategorias.Categoria;
+import criteriosCategorias.CriterioCategorizacion;
+
 public class Producto {
 	private String descripcion;
 	private float valor = 0;
-	
+	public ArrayList<Categoria> categorias = new ArrayList<Categoria>();
 	public Producto(String descripcion, float valor) {
 		super();
 		this.descripcion = descripcion;
@@ -20,5 +25,22 @@ public class Producto {
 	}
 	public void setValor(float valorIngresado) {
 		this.valor = valorIngresado;
+	}
+	private boolean buscarCriterio(CriterioCategorizacion criterio) {
+		boolean encontrado = false;
+			for(int i=0; i<categorias.size();i++) {
+				Categoria cat = categorias.get(i);
+				if(criterio == cat.getCriterio()) {
+					encontrado = true;
+					break;
+				}
+			}
+	return encontrado;
+	}
+	
+	public void agregarCategoria(Categoria categoria) {
+		//Puede estar asociado a sólo una categoría de cada criterio
+		if(categorias.isEmpty() || !this.buscarCriterio(categoria.getCriterio()))
+		categorias.add(categoria);
 	}
 }

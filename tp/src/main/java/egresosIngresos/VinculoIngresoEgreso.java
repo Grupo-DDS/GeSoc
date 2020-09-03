f77f6b5cfffd830da0118ec71cfe406d6ee25b3c
Julián García <49066529+juliangarcia45@users.noreply.github.com>
2020-09-03 20:40:52 -0300

    justificarIngreso
    Todos los cambios están comentados porque no son definitivos

--- tp/src/main/java/egresosIngresos/VinculoIngresoEgreso.java
+++ tp/src/main/java/egresosIngresos/VinculoIngresoEgreso.java
@@ -1,16 +1,33 @@
 package egresosIngresos;
 
+import java.util.ArrayList;
+
 public class VinculoIngresoEgreso {
 	private OperacionIngreso ingreso;
 	private ArrayList<OperacionEgreso> egresos=new ArrayList<OperacionEgreso>();
-	 
+	//private PeriodoAceptabilidadEgreso periodoAceptabilidad;
+	
 	public OperacionIngreso getIngreso() {
 		return ingreso;
 	}
 	public void setIngreso(OperacionIngreso ingreso) {
 		this.ingreso = ingreso;
 	}
-	public void setEgreso(OperacionEgreso egreso) {
+	public void agregarEgreso(OperacionEgreso egreso) {
+		//if((egreso.getFechaOperacion().compareTo(periodoAceptabilidad.getFechaInicioPeriodo()))>0 && (egreso.getFechaOperacion().compareTo(periodoAceptabilidad.getFechaFinPeriodo()))<0) {
 		egresos.add(egreso);
+		//}
 	}
+	/*private float sumatoriaEgresos() {
+		float sum=0;
+		for (OperacionEgreso unEgreso:egresos) {
+			sum+=unEgreso.getValorDeEgreso();
+		}
+		return sum;
+	}
+	public  boolean justificarIngreso() {
+		return ingreso.getMontoTotal() >= this.sumatoriaEgresos();
+	}
+	*/
 }
+

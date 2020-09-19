7b2020ab4f77bba63ba546fd90fb674a8ac4477b
Julián García <49066529+juliangarcia45@users.noreply.github.com>
2020-09-19 18:50:26 -0300

    preeliminar ingreso, egreso, direccion, doc comercial

--- tp/src/main/java/egresosIngresos/Direccion.java
+++ tp/src/main/java/egresosIngresos/Direccion.java
@@ -1,8 +1,73 @@
 package egresosIngresos;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="DIRECCIONES")
 public class Direccion {
-	String calle;
-	String localidad;
-	String piso;
-	String provincia;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="ID_DIRECCION")
+	private Long id;
+	@Column(name="CALLE")
+	private String calle;
+	@Column(name="LOCALIDAD")
+	private String localidad;
+	@Column(name="PISO")
+	private String piso;
+	@Column(name="PROVINCIA")
+	private String provincia;
+	
+	
+	@Override
+	public String toString() {
+		return "Direccion [id=" + id + ", calle=" + calle + ", localidad=" + localidad + ", piso=" + piso
+				+ ", provincia=" + provincia + "]";
+	}
+	
+	public Direccion(Long id, String calle, String localidad, String piso, String provincia) {
+		super();
+		this.id = id;
+		this.calle = calle;
+		this.localidad = localidad;
+		this.piso = piso;
+		this.provincia = provincia;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getCalle() {
+		return calle;
+	}
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+	public String getLocalidad() {
+		return localidad;
+	}
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+	public String getPiso() {
+		return piso;
+	}
+	public void setPiso(String piso) {
+		this.piso = piso;
+	}
+	public String getProvincia() {
+		return provincia;
+	}
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+	
+	
 }

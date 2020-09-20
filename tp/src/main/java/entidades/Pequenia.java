e64141d9b3fa51b4c675fc8fb448e9b3e48c5459
Soledad <48894797+soledadAmore@users.noreply.github.com>
2020-09-20 00:56:05 -0300

    Update Pequenia.java

--- tp/src/main/java/entidades/Pequenia.java
+++ tp/src/main/java/entidades/Pequenia.java
@@ -1,5 +1,31 @@
 package entidades;
 
-public class Pequenia extends Empresa{
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity(name="Pequenia")
+public class Pequenia extends Empresa{
+	//@Id
+	//@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int idPequenia;
+	private static Pequenia instance = null;
+	
+	public Pequenia() {}
+
+	public int getIdPequenia() {
+		return idPequenia;
+	}
+
+	public void setIdPequenia(int idPequenia) {
+		this.idPequenia = idPequenia;
+	}
+	
+	public static Pequenia getInstance() {
+		if (instance == null)
+			instance = new Pequenia();
+		return instance;
+	}
+	
 }

0769e50e1681bae419a0c97c68c108c7bea45638
Soledad <48894797+soledadAmore@users.noreply.github.com>
2020-09-20 00:49:21 -0300

    Update EntidadBase.java

--- tp/src/main/java/entidades/EntidadBase.java
+++ tp/src/main/java/entidades/EntidadBase.java
@@ -1,5 +1,29 @@
 package entidades;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name="EntidadBase")
 public class EntidadBase extends Entidad{
+	//@Id
+	//@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int idEntidadBase;
 	String descripcion;
+	public EntidadBase() {}
+	
+	public int getIdEntidadBase() {
+		return idEntidadBase;
+	}
+	public void setIdEntidadBase(int idEntidadBase) {
+		this.idEntidadBase = idEntidadBase;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
 }

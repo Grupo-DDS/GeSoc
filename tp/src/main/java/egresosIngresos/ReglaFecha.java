ada71c71384ce7a013d6287611b9d735fd2785d2
Julián García <49066529+juliangarcia45@users.noreply.github.com>
2020-09-22 18:42:04 -0300

    update

--- tp/src/main/java/egresosIngresos/ReglaFecha.java
+++ tp/src/main/java/egresosIngresos/ReglaFecha.java
@@ -3,7 +3,9 @@ package egresosIngresos;
 public class ReglaFecha extends ReglaVinculacion {
 
 	public boolean esVinculable(OperacionIngreso ingresoAVincular, OperacionEgreso egresoAVincular) {
-		// TODO Auto-generated method stub
+		if(egresoAVincular.getFechaOperacion().compareTo(ingresoAVincular.getFechaInicio())>0 && egresoAVincular.getFechaOperacion().compareTo(ingresoAVincular.getFechaFin())<0 &&ingresoAVincular.getMontoTotal()>=egresoAVincular.getValorDeEgreso()) {
+			return true;
+		}
 		return false;
 	}
 

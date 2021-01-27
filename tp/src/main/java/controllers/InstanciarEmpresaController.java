ea334e28ef95de35469ff1ab36fde18028a07261
Juan Carassai <49323627+JuanCarassai@users.noreply.github.com>
2021-01-27 15:38:38 -0300

    Arreglo el boton de ver entidades base

--- tp/src/main/java/controllers/InstanciarEmpresaController.java
+++ tp/src/main/java/controllers/InstanciarEmpresaController.java
@@ -5,10 +5,12 @@ import static app.RequestUtil.getQueryFecha;
 import static app.RequestUtil.getQueryMontoTotal;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import app.Path;
 import app.ViewUtil;
+import comprasPresupuestos.PresupuestoDetallado;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -16,6 +18,8 @@ import spark.Route;
 public class InstanciarEmpresaController {
 	public static Route instanciarEmpresa = (Request request, Response response) -> {
 		Map<String, Object> model = new HashMap<>();
+		List<PresupuestoDetallado> presupuestos = PresupuestoDetallado.obtenerTodosEnBD();
+		model.put("presupuestos", presupuestos);
 		//LoginController.ensureUserIsLoggedIn(request, response);
 
 		return ViewUtil.render(request, model, Path.Template.INSTANCIAR_EMPRESA);

ebab7ab69053514695cacd5dd0842ad04608c2c6
Juan Carassai <49323627+JuanCarassai@users.noreply.github.com>
2021-02-06 16:31:34 -0300

    update

--- src/main/java/egresosIngresos/Proveedor.java
+++ src/main/java/egresosIngresos/Proveedor.java
@@ -8,7 +8,7 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.OneToOne;
 
-@Entity (name = "PROVEEDOR")
+@Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class Proveedor {
 	@Id
